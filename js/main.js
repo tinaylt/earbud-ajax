a0ce87e4d6a19d23fadfc575a7b5747473ee56fd
tinaylt <144871562+tinaylt@users.noreply.github.com>
2024-11-30 21:05:17 -0500

    fetched from the API and added loading spinner and error Message in JS
    This is to fetch data from InfoBoxes and materials API, and copied the data to annotation box and material list. Loading spinners have been added in html and page and remove class hidden when it ioading or shows error. Error Message has also been placed.

--- js/main.js
+++ js/main.js
@@ -6,94 +6,96 @@
   const materialTemplate = document.querySelector("#material-template");
   const materialList = document.querySelector("#material-list");
 
+  const loaders = document.querySelectorAll(".loader");
+  const annotations = document.querySelectorAll(".HotspotAnnotation");
+  const loaderMaterial = document.querySelector("#loader-material");
+
   //This information needs to be removed then pulled with an AJAX Call using the Fetch API
   //this is the api url https://swiftpixel.com/earbud/api/infoboxes"
 
-  const infoBoxes = [
-    {
-      title: 'Noise-cancelling microphones',
-      text: 'Noise-cancelling microphones and a rear copper shield are optimally placed to quickly detect outside noises, working together to counter noise before it disturbs your experience.',
-      image: 'images/ear-piece.jpg'
-    },
-    {
-      title: 'Comfortable fit',
-      text: 'Three pairs of ultra comfortable silicone tips are included. The tips create an acoustic seal that blocks outside audio and secures the earbuds in place.',
-      image: 'images/ear-piece.jpg'
-    },
-    {
-      title: '360 AUDIO',
-      text: '360 Audio places sound all around you, while Dolby Head Tracking™ technology delivers an incredible three-dimensional listening experience.',
-      image: 'images/ear-piece.jpg'
-    },
-    {
-      title: 'Ultra Fast Charging',
-      text: 'Charge your earbuds in 30 minutes or less with our hyper charging technology.',
-      image: 'images/ear-piece.jpg'
-    },
-  ];
+  function loadInfoBoxes() {
+   
+    fetch("https://swiftpixel.com/earbud/api/infoboxes")
+    .then(response => response.json())
+    .then(infoBoxes => {
+      console.log(infoBoxes);
+
+      infoBoxes.forEach((infoBox, index) => {
+        let selected = document.querySelector(`#hotspot-${index + 1}`);
+  
+        const titleElement = document.createElement('h2');
+        titleElement.textContent = infoBox.heading;
+  
+        const textElement = document.createElement('p');
+        textElement.textContent = infoBox.description;
+  
+        selected.appendChild(titleElement);
+        selected.appendChild(textElement);
+
+      });
+
+    })
+    .catch(error => {
+      console.log(error);
+
+      loaders.forEach(loader => {
+        loader.classList.toggle("hidden");
+        annotations.innerHTMl = "";
+      });
+
+      annotations.forEach(annotation => {
+        const errorMessage = document.createElement("p");
+        errorMessage.textContent = "Oops, it's looks like something went wrong. please check your internet connection or try again later.";
+        errorMessage.style.color = "#acacac";
+        errorMessage.style.fontSize = "13px";
+
+        annotation.appendChild(errorMessage);
+      })
+
+    })
+  }
+
+  loadInfoBoxes()
 
   //This information needs to be removed then pulled with an AJAX Call using the Fetch API
   //this is the api url https://swiftpixel.com/earbud/api/materials"
 
-  const materialListData = [
-    {
-      heading: "Precision-Crafted Polymers",
-      description: "Our earbuds are meticulously molded from high-quality plastics, ensuring a blend of elegance, comfort, and resilience that's second to none."
-    },
-    {
-      heading: "Luxurious Silicone Harmony",
-      description: "Our uniquely engineered ear tips are cocooned in plush silicone, delivering an opulent embrace for your ears, ensuring an unrivaled fit and exquisite audio experience."
-    },
-    {
-      heading: "Rubberized Cables",
-      description: "Experience the unparalleled freedom of movement with our flexible rubber cables that promise durability without compromise."
-    },
-    {
-      heading: "Enhanced Comfort Sensors",
-      description: "A touch of magic in the form of built-in microphones and sensors empowers your earbuds to obey your every command, making your audio journey seamless and enchanting."
-    },
-    {
-      heading: "Artistic Mesh Guard",
-      description: "Shielded by artful mesh screens, our speakers remain untarnished, keeping your listening experience pristine."
-    }
-  ];
-
-  //functions
-  function loadInfoBoxes() {
-
-    //make AJAX call here
-
-    infoBoxes.forEach((infoBox, index) => {
-      let selected = document.querySelector(`#hotspot-${index + 1}`);
-
-      const titleElement = document.createElement('h2');
-      titleElement.textContent = infoBox.title;
-
-      const textElement = document.createElement('p');
-      textElement.textContent = infoBox.text;
-
-      selected.appendChild(titleElement);
-      selected.appendChild(textElement);
-    });
-  }
-  loadInfoBoxes();
-
-
   function loadMaterialInfo() {
 
-    materialListData.forEach(material => {
-    //clone the template, make a copy of that template//
-    const clone = materialTemplate.content.cloneNode(true);
-    //populate with data//
-    const materialHeading = clone.querySelector(".material-heading"); 
-    materialHeading.textContent = material.heading;
+    fetch("https://swiftpixel.com/earbud/api/materials")
+    .then(response => response.json())
+    .then(materials => {
+      console.log(materials);
 
-    const materialDescription = clone.querySelector(".material-description");
-    materialDescription.textContent = material.description;
+      materials.forEach(material => {
+        //clone the template, make a copy of that template//
+        const clone = materialTemplate.content.cloneNode(true);
+        //populate with data//
+        const materialHeading = clone.querySelector(".material-heading"); 
+        materialHeading.textContent = material.heading;
+    
+        const materalDescription = clone.querySelector(".material-description");
+        materalDescription.textContent = material.description;
+    
+        materialList.appendChild(clone);
+        });
 
-    materialList.appendChild(clone);
 
-    });
+
+    })
+    .catch(error => {
+      console.log(error)
+
+      loaderMaterial.classList.toggle("hidden");
+      materialList.innerHTML = "";
+
+      const errorMessage = document.createElement("p");
+      errorMessage.textContent = "Oops, it's looks like something went wrong. please check your internet connection or try again later.";
+      errorMessage.style.color = "#acacac";
+
+      materialList.appendChild(errorMessage)
+    })
+
     }
 
     loadMaterialInfo();
